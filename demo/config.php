4fff667bed02f1e091de17332493d8ef48839e00
tyutjohn <36732345+tyutjohn@users.noreply.github.com>
2019-02-14 16:10:19 +0800

    更新易班接口

---
+++ demo/config.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+	 * 配置文件(网站接入)
+	 */
+	$config = array(
+	    'AppID'     => '057c06002a028e4f',  //此处填写你的appid
+	    'AppSecret' => 'ed2b81fa8022becd1f0476024b83a64c',  //此处填写你的AppSecret
+	    'CallBack'  => 'http://tyutjohn.com/ybdemo/demo/authorize.php',  //此处填写你的授权回调地址
+	);
